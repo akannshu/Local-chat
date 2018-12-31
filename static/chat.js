953ff902f9b4c1fd6d7084a3c28d9632096b8f08
Akanshu <37223446+akannshu@users.noreply.github.com>
2019-01-01 01:15:43 +0530

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,13 +1,12 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('192.168.43.214:8888');
 
-// Query DOM
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
       output = document.getElementById('output'),
       feedback = document.getElementById('feedback');
 
-// Emit events
+
 btn.addEventListener('click', function(){
     socket.emit('chat', {
         message: message.value,
@@ -20,7 +19,7 @@ message.addEventListener('keypress', function(){
     socket.emit('typing', handle.value);
 })
 
-// Listen for events
+
 socket.on('chat', function(data){
     feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
